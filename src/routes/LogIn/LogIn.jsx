c87e93d3de99f8f8ab49ec04e814d6a75fa754b5
Eugene Bondar <bondar.e@nixsolutions.com>
2020-04-27 13:08:32 +0300

    fix formationg

--- src/routes/LogIn/LogIn.jsx
+++ src/routes/LogIn/LogIn.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { useFormik } from 'formik';
 import { Redirect } from 'react-router-dom';
-import { validateLogin } from '../../utils/validations';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
+
+import { validateLogin } from '../../utils/validations';
 
 import './LogIn.scss';
 
