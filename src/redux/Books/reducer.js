8b1a23a9a5881cd5ddd725f142e9c5aa52ddcd00
Vladislav Dordurov <vladislav.dordurov@nixsolutions.com>
2020-04-29 15:18:23 +0300

    Pagination-07

--- src/redux/Books/reducer.js
+++ src/redux/Books/reducer.js
@@ -1,8 +1,10 @@
-import { FETCH_PRODUCTS_BEGIN, FETCH_PRODUCTS_SUCCESS } from './actionTypes';
+import { FETCH_PRODUCTS_BEGIN, FETCH_PRODUCTS_SUCCESS, PAGE_INCREMENT, CHECK_SHOW_MORE } from './actionTypes';
 
 const initialState = {
   list: [],
   isFetching: false,
+  page: 1,
+  showMore: true,
 };
 
 export const cardsReducer = (state = initialState, action) => {
@@ -12,13 +14,23 @@ export const cardsReducer = (state = initialState, action) => {
     case FETCH_PRODUCTS_SUCCESS:
       return {
         ...state,
-        list: payload.products,
+        list: [...state.list, ...payload.products],
         isFetching: false,
       };
     case FETCH_PRODUCTS_BEGIN:
       return {
         ...state,
         isFetching: true,
+      };
+    case PAGE_INCREMENT:
+      return {
+        ...state,
+        page: state.page + 1,
+      };
+    case CHECK_SHOW_MORE:
+      return {
+        ...state,
+        showMore: payload.showMore,
       };
     default:
       return state;
